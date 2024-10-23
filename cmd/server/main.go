32c386da386fb7445afa09e30605b35be8153c1b
Anton Kuzmin <plasmatrip@gmail.com>
2024-10-24 00:58:46 +0300

    feat: add server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle(`/update/`, UpdateHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
