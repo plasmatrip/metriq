76d5399d2efc71eade4e283ee8bc5cdc4f300afa
plasma_trip <plasmatrip@gmail.com>
2024-10-29 17:35:36 +0300

    feat: add config test

---
+++ internal/agent/config_test.go
@@ -0,0 +1,73 @@
+package agent
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig_ParseAddress(t *testing.T) {
+	tests := []struct {
+		name  string
+		value string
+		want  string
+	}{
+		{
+			name:  "Valid address",
+			value: "server.com:8989",
+			want:  "server.com:8989",
+		},
+		{
+			name:  "Empty port number",
+			value: "server.com:",
+			want:  "localhost:8080",
+		},
+		{
+			name:  "Empty host name",
+			value: ":8574",
+			want:  "localhost:8080",
+		},
+		{
+			name:  "Empty address",
+			value: "",
+			want:  "localhost:8080",
+		},
+		{
+			name:  "Only colon",
+			value: ":",
+			want:  "localhost:8080",
+		},
+	}
+	config := new(Config)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			config.Host = test.value
+			parseAddress(config)
+			assert.Equal(t, test.want, config.Host)
+		})
+	}
+}
+
+func TestConfig_NewConfig(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+		want Config
+	}{
+		{
+			name: "Valid config",
+			args: []string{"-a", "server.com:8585"},
+			want: Config{Host: "server.com:8585", PollInterval: 2, ReportInterval: 10},
+		},
+	}
+	fs := flag.NewFlagSet("test", flag.ContinueOnError)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fs.String("a", "localhost:8080", "Server address host:port")
+			fs.Parse(test.args)
+			config := NewConfig()
+			assert.Equal(t, test.want, *config)
+		})
+	}
+}
