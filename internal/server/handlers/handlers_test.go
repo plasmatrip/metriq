ae9c7c66b28e6d58bd9e6c35e26905f0abda366b
Anton Kuzmin <plasmatrip@gmail.com>
2025-02-18 19:14:30 +0300

    feat: iter18 release

--- internal/server/handlers/handlers_test.go
+++ internal/server/handlers/handlers_test.go
@@ -8,7 +8,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/plasmatrip/metriq/internal/logger"
+	"github.com/plasmatrip/metriq/internal/server/compress"
 	"github.com/plasmatrip/metriq/internal/server/config"
 	"github.com/plasmatrip/metriq/internal/storage/mem"
 	"github.com/plasmatrip/metriq/internal/types"
@@ -281,4 +284,100 @@ func BenchmarkUpdateHandler(b *testing.B) {
 		assert.NoError(b, err)
 		defer res.Body.Close()
 	}
+}
+
+func ExampleNewHandlers() {
+	// Initialize the config
+	config, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the storage
+	storage := mem.NewStorage()
+
+	// Initialize the logger
+	logger, err := logger.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the handlers
+	handlers := NewHandlers(storage, *config, logger)
+
+	// Use the handlers
+	// The chi router is used to handle the routing
+	r := chi.NewRouter()
+
+	r.Route("/ping", func(r chi.Router) {
+		r.Get("/", handlers.Ping)
+	})
+}
+
+func Example() {
+	// This function demonstrates an example of initializing and using the main components
+	// of the application, including configuration, storage, logger, and request handlers.
+	//
+	// It first initializes the server configuration using the config.NewConfig function.
+	// If the configuration cannot be initialized, the program will panic, indicating a
+	// critical failure to set up the server's operational parameters.
+	//
+	// Next, it sets up the in-memory storage by calling mem.NewStorage. This storage acts
+	// as the repository for storing and retrieving metrics, providing a simple and fast
+	// storage solution during the server's runtime.
+	//
+	// The logger is initialized using logger.NewLogger, which sets up the logging
+	// infrastructure for the application. If the logger fails to initialize, the program
+	// will panic, as logging is crucial for monitoring and debugging the server.
+	//
+	// With the configuration, storage, and logger ready, the function proceeds to
+	// instantiate the handlers using the NewHandlers function. These handlers will manage
+	// incoming HTTP requests, utilizing the initialized components to handle various
+	// operations such as updating and retrieving metrics.
+	//
+	// Although the example does not complete the setup of the HTTP server, it outlines the
+	// typical initialization steps and highlights the importance of each component in the
+	// server's lifecycle.
+
+	// Initialize the config
+	config, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the storage
+	storage := mem.NewStorage()
+
+	// Initialize the logger
+	logger, err := logger.NewLogger()
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize the handlers
+	handlers := NewHandlers(storage, *config, logger)
+
+	// Use the handlers
+	// The chi router is used to handle the routing
+	r := chi.NewRouter()
+
+	r.Use(compress.WithCompression(logger), logger.WithLogging)
+
+	r.Mount("/debug", middleware.Profiler())
+
+	r.Route("/update", func(r chi.Router) {
+		r.Post("/", handlers.JSONUpdate)
+	})
+	r.Route("/updates", func(r chi.Router) {
+		r.Post("/", handlers.JSONUpdates)
+	})
+	r.Route("/value", func(r chi.Router) {
+		r.Post("/", handlers.JSONValue)
+	})
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", handlers.Update)
+	r.Get("/value/{metricType}/{metricName}", handlers.Value)
+	r.Get("/", handlers.Metrics)
+	r.Route("/ping", func(r chi.Router) {
+		r.Get("/", handlers.Ping)
+	})
 }
